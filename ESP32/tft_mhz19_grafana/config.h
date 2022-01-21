38ad1c652eea7247663a7794dec1ec145a52b791
reery <reery7@yahoo.de>
2022-01-21 09:38:24 +0100

    Add files via upload

---
+++ ESP32/tft_mhz19_grafana/config.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID     "SWAG-NET 2.4"
+#define WIFI_PASSWORD "rdmNbz.11kYOLO"
+
+// For more information on where to get these values see: https://github.com/grafana/diy-iot/blob/main/README.md#sending-metrics
+#define GC_URL "prometheus-prod-01-eu-west-0.grafana.net"
+#define GC_PATH "/api/prom/push"
+#define GC_PORT 443
+#define GC_USER "308206"
+#define GC_PASS "eyJrIjoiZDkyYjcxZmZiZDkzYmNmZWJmZjI3OTBkM2ZjNjhlYmRkMWNiZmM3NyIsIm4iOiJncmFmYW5hX3Byb20iLCJpZCI6NTkxMzU4fQ=="
